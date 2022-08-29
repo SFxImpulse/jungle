82a21e88e4b71789ca8befd80d071d2d323a2609
SFxImpulse <braverman.david.business@gmail.com>
2022-08-29 21:35:31 +0000

    Add spec files for testing models.

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,84 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    context "Save Validation" do
+      it "saves when required fields are filled" do
+        @category = Category.find_or_create_by! name: "Trees"
+        @product = @category.products.create!(
+          name: "Orange Lily",
+          description: "A gorgeous orange flower, introduced to North America some time ago, no grows in many unexpected places.",
+          image: nil,
+          quantity: 5,
+          price: 7.50,
+        )
+
+        expect(@product.id).to eql(@product.id)
+      end
+    end
+
+    context "Name Presence Validation" do
+
+      it "validates that a name does exist when a name is set" do
+        @category = Category.find_or_create_by! name: "Trees"
+        @product = @category.products.create!(
+          name: "Orange Lily",
+          description: "A gorgeous orange flower, introduced to North America some time ago, no grows in many unexpected places.",
+          image: nil,
+          quantity: 5,
+          price: 7.50,
+        )
+
+        expect(@product.name).to be_present
+      end
+    end
+
+    context "Price Presence Validation" do
+
+      it "validates that a price does exist when a price is set" do
+        @category = Category.find_or_create_by! name: "Trees"
+        @product = @category.products.create!(
+          name: "Orange Lily",
+          description: "A gorgeous orange flower, introduced to North America some time ago, no grows in many unexpected places.",
+          image: nil,
+          quantity: 5,
+          price: 7.50,
+        )
+
+        expect(@product.price).to be_present
+      end
+    end
+
+    context "Quantity Presence Validation" do
+
+      it "validates that a quantity does exist when a quantity is set" do
+        @category = Category.find_or_create_by! name: "Trees"
+        @product = @category.products.create!(
+          name: "Orange Lily",
+          description: "A gorgeous orange flower, introduced to North America some time ago, no grows in many unexpected places.",
+          image: nil,
+          quantity: 5,
+          price: 7.50,
+        )
+
+        expect(@product.quantity).to be_present
+      end
+    end
+
+    context "Category Presence Validation" do
+
+      it "validates that a category does exist when a category is set" do
+        @category = Category.find_or_create_by! name: "Trees"
+        @product = @category.products.create!(
+          name: "Orange Lily",
+          description: "A gorgeous orange flower, introduced to North America some time ago, no grows in many unexpected places.",
+          image: nil,
+          quantity: 5,
+          price: 7.50,
+        )
+
+        expect(@product.category).to be_present
+      end
+    end
+  end
+end
